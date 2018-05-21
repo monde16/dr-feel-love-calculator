7173097a39b0f60b25fe5fc6e30cc67735df2657
Monde Ngalonkulu <monde.ngalonkulu@nimbletech.co.za>
2018-05-21 13:29:00 +0200

    Implemented app functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,33 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  name1: string = '';
+  name2: string = '';
+  results: number;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  reset(): void {
+    this.results = undefined;
+  }
+
+  calculate(): void {
+    console.log("************************");
+    let s1 = this.name1.toLowerCase(), s2 = this.name2.toLowerCase();
+    console.log(`s1=${s1}, s2=${s2}`);
+    for (let k of s1) {
+      if (s2.indexOf(k) > -1) {
+        s1 = s1.replace(k, '');
+        s2 = s2.replace(k, '');
+      }
+    }
+    console.log(`s1=${s1}, s2=${s2}`);
+    let l = this.name1.length + this.name2.length;
+    console.log(`l=${l}`);
+    console.log(`s1 + s2 = ${s1.length + s2.length}`);
+    this.results = ((l - s1.length - s2.length) / l) * 100;
+    console.log(`results = ${this.results}`);
+  }
 }
